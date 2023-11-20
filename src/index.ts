041cacd05f8472f32991b5983d52a92e1b4d7014
DanielRicra <daniel.int@outlook.com>
2023-11-20 14:54:41 -0500

    Implement the app entry

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,13 @@
+import { AppRoutes } from "./presentation/routes";
+import { Server } from "./presentation/server";
 
+(() => {
+  main();
+})();
+
+async function main() {
+  new Server({
+    port: 3000,
+    routes: AppRoutes.routes,
+  }).start();
+}
