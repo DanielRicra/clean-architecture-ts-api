725a3c35d3b18052a94e0d8cba3ac3f308008282
DanielRicra <daniel.int@outlook.com>
2023-11-21 10:53:17 -0500

    SignUp implemented

--- src/presentation/auth/controller.ts
+++ src/presentation/auth/controller.ts
@@ -1,13 +1,21 @@
-import { Request, Response } from "express";
+import type { RequestHandler } from "express";
+import { SignUpUserDTO } from "../../domain";
 
 export class AuthController {
   constructor() {}
 
-  signUpUser = (req: Request, res: Response) => {
-    res.json("signUpUser controller");
+  signUpUser: RequestHandler = (req, res) => {
+    const [error, userDTO] = SignUpUserDTO.create(req.body);
+
+    if (error) {
+      res.status(400).json({ error });
+      return;
+    }
+
+    res.status(201).json(userDTO);
   };
 
-  signInUser = (req: Request, res: Response) => {
+  signInUser: RequestHandler = (req, res) => {
     res.json("signInUser controller");
   };
 }
