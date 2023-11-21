4aa07195538038f6cd447a67de8bf3ab939365fb
DanielRicra <daniel.int@outlook.com>
2023-11-21 10:52:46 -0500

    Add json and urlencoded middlewares

--- src/presentation/server.ts
+++ src/presentation/server.ts
@@ -17,8 +17,11 @@ export class Server {
   }
 
   async start() {
+    this.app.use(express.json());
+    this.app.use(express.urlencoded({ extended: true }));
+    
     this.app.use(this.routes);
-
+    
     this.app.listen(this.port, () => {
       console.log(`Server running on port http://localhost:${this.port}`);
     });
