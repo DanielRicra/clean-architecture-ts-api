14efc4a833dbac3d1f58b458adc769cd33dd7beb
DanielRicra <daniel.int@outlook.com>
2023-11-23 14:52:31 -0500

    Update jwt. use secret seed from env
    Add validateToken method

--- src/config/jwt.ts
+++ src/config/jwt.ts
@@ -1,4 +1,7 @@
 import jwt from "jsonwebtoken";
+import { envs } from "./envs";
+
+const JWT_SEED = envs.JWT_SEED;
 
 export class JwtAdapter {
   static async generateToken(
@@ -6,16 +9,21 @@ export class JwtAdapter {
     duration: string = "2h"
   ): Promise<string | null> {
     return new Promise((resolve) => {
-      jwt.sign(
-        payload,
-        "SECRET_SEED",
-        { expiresIn: duration },
-        (error, token) => {
-          if (error) return resolve(null);
+      jwt.sign(payload, JWT_SEED, { expiresIn: duration }, (error, token) => {
+        if (error) return resolve(null);
 
-          resolve(token!);
-        }
-      );
+        resolve(token!);
+      });
+    });
+  }
+
+  static validateToken<T>(token: string): Promise<T | null> {
+    return new Promise((resolve) => {
+      jwt.verify(token, JWT_SEED, (error, decoded) => {
+        if (error) return resolve(null);
+
+        resolve(decoded as T);
+      });
     });
   }
 }
