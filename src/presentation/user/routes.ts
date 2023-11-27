5c29fbede5e99d99bf1e370c20b1eb1fe5f0572d
DanielRicra <daniel.int@outlook.com>
2023-11-27 12:02:21 -0500

    Update user routes
    - Add datasource and repository instance, pass both to controller

--- src/presentation/user/routes.ts
+++ src/presentation/user/routes.ts
@@ -1,12 +1,16 @@
 import { Router } from "express";
 import { UserController } from "./controller";
 import { AuthMiddleware } from "../middlewares/auth.middleware";
+import { UserDatasourceImpl, UserRepositoryImpl } from "../../infrastructure";
 
 export class UserRouter {
   static get routes(): Router {
     const router = Router();
 
-    const controller = new UserController();
+    const datasource = new UserDatasourceImpl();
+    const repository = new UserRepositoryImpl(datasource);
+
+    const controller = new UserController(repository);
 
     router.get("/", [AuthMiddleware.validateJwt], controller.getUsers);
 
