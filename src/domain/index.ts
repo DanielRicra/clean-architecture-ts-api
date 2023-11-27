59688569102c124745e78a1dfa89cf32c7531024
DanielRicra <daniel.int@outlook.com>
2023-11-27 11:50:03 -0500

    Update the export of user related files:
    - Datasource, repository

--- src/domain/index.ts
+++ src/domain/index.ts
@@ -1,8 +1,15 @@
 export * from "./dtos/auth/signup-user.dto";
 export * from "./dtos/auth/signin-user.dto";
+
 export * from "./entities/user.entity";
+
 export * from "./errors/custom.error";
+
 export * from "./datasources/auth.datasource";
+export * from "./datasources/user.datasource";
+
 export * from "./repositories/auth.repository";
+export * from "./repositories/user.repository";
+
 export * from "./use-cases/auth/signup-user.use-case";
 export * from "./use-cases/auth/signin-user.use-case";
