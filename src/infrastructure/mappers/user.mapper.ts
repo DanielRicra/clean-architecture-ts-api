0bb508ff6e8447f4af7c5be96650108be02d3257
DanielRicra <daniel.int@outlook.com>
2023-11-27 18:22:15 -0500

    No return password in user mapper

--- src/infrastructure/mappers/user.mapper.ts
+++ src/infrastructure/mappers/user.mapper.ts
@@ -2,14 +2,13 @@ import { CustomError, UserEntity } from "../../domain";
 
 export class UserMapper {
   static userEntityFromObject(object: { [key: string]: any }) {
-    const { id, name, email, password, roles } = object;
+    const { id, name, email, roles, imgUrl } = object;
 
     if (!id) throw CustomError.badRequest("Missing id");
     if (!name) throw CustomError.badRequest("Missing name");
-    if (!password) throw CustomError.badRequest("Missing password");
     if (!email) throw CustomError.badRequest("Missing email");
     if (!roles) throw CustomError.badRequest("Missing roles");
 
-    return new UserEntity(id, name, email, password, roles);
+    return new UserEntity(id, name, email, roles, undefined, imgUrl);
   }
 }
