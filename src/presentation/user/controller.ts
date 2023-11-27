c41ece889474e71a8d3ed5d7f890e7e3f340c7aa
DanielRicra <daniel.int@outlook.com>
2023-11-27 12:00:46 -0500

    Update user controller
    - Implement `handleError` private method.
    
    - Replace the logic of `getUsers` with `GetUser` use-case

--- src/presentation/user/controller.ts
+++ src/presentation/user/controller.ts
@@ -1,13 +1,22 @@
-import { RequestHandler } from "express";
-import prisma from "../../data/sqlite/sqlite-database";
+import { RequestHandler, Response } from "express";
+import { CustomError, UserRepository } from "../../domain";
+import { GetUsers } from "../../domain/use-cases/user/get-users.use-case";
 
 export class UserController {
+  constructor(private readonly userRepository: UserRepository) {}
+
+  private handleError = (error: unknown, res: Response) => {
+    if (error instanceof CustomError) {
+      return res.status(error.statusCode).json({ error: error.message });
+    }
+
+    return res.status(500).json({ error: "Internal server error" });
+  };
+
   getUsers: RequestHandler = (req, res) => {
-    prisma.user
-      .findMany()
-      .then((users) => res.json({ users }))
-      .catch((error) =>
-        res.status(500).json({ error: "Server internal error" })
-      );
+    new GetUsers(this.userRepository)
+      .execute()
+      .then((data) => res.json(data))
+      .catch((error) => this.handleError(error, res));
   };
 }
